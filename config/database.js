655beeb031761e22fc79a0a9e914ae282aa4cfa2
izalsye <nurwibowofaisal@gmail.com>
2025-05-12 18:17:41 +0700

    connection

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('u1777408_bot', 'u1777408_larva', 'vps01larva', {
-  host: 'localhost',
+  host: '185.229.118.120',
   dialect: 'mysql',
   logging: false
 });
